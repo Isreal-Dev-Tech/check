65493a3b3c8c4533e2f6143503a8a4f361b513cb
Isreal-Dev-Tech <isrealdevtech@gmail.com>
2026-05-12 16:30:16 +0100

    Update TikTokLive import path in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { TikTokLive } = require('@tiktool/live');
+const { TikTokLive } = require('./node_modules/@tiktool/live/dist/index.js');
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
