74416141d0fa9bf24a2fab214fdf3df1bc902e8c
Isreal-Dev-Tech <isrealdevtech@gmail.com>
2026-05-12 21:31:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const io = new Server(server);
 // SETTINGS
 // ===============================
 const TIKTOOL_API_KEY = "tk_91ec88c2870958d10d58fbcfe4e73840d018705e201a96c1";
-const TARGET_USERNAME = "mynameanabel"; // Put your username here
+const TARGET_USERNAME = "realjadrolita"; // Put your username here
 const POINTS_PER_LAP = 50;
 
 // All country data is now right here - no config.json needed!
